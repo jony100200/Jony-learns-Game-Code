e58b2175bd241b1904058bc9eacfad25d58a14ce
Jony <gamingbir@gmail.com>
2020-07-13 19:38:00 -0400

    learnt module lesson 1 n 2

---
+++ Learning_01_01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_01_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("I am learning better C# programming");
+        }
+    }
+}
