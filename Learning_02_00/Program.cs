862e62b2a5e6e87c6df2db8f8d660ca56d6729ba
Jony <gamingbir@gmail.com>
2020-07-14 09:54:44 -0400

    completed 2_00

---
+++ Learning_02_00/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_02_00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Gender: \n Male, Female");
+
+            string command = Console.ReadLine();
+
+            if (command=="Male")
+            {
+                Console.WriteLine("Hello Sir");
+            }else if (command == "Female") 
+            {
+                Console.WriteLine("Hello Madame");
+            }
+            Console.ReadLine();
+        }
+    }
+}
