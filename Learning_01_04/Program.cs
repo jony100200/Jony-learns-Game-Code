37773d1810112eca45284f63d85c0e65110271ad
Jony <gamingbir@gmail.com>
2020-07-13 22:01:12 -0400

    finished the 1st section of the course

---
+++ Learning_01_04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_01_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;//varialbe w/type n identifier
+            name = "Jony";//assign  data to our variable which stores in a memory location.
+
+            Console.WriteLine("Hello " + name);//using variable to print our name
+            Console.ReadLine();
+        }
+    }
+}
