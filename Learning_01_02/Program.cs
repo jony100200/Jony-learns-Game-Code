323978aabff499ec5fce72e14bfbf6028128a8e0
Jony <gamingbir@gmail.com>
2020-07-13 20:37:02 -0400

    code changes

--- Learning_01_02/Program.cs
+++ Learning_01_02/Program.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Learning_01_02
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Woah we learning better game programming in C#");
-            Console.Write(" And its seems fun");
-            Console.Read();
-            //difference of write and writeline is that writeline makes a new line
-        }
-    }
-}
+//namespace Learning_01_02
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.Write("Woah we learning better game programming in C#");
+//            Console.Write(" And its seems fun");
+//            Console.Read();
+//            //difference of write and writeline is that writeline makes a new line
+//        }
+//    }
+//}
--- Learning_01_02/challenge.cs
+++ Learning_01_02/challenge.cs
@@ -8,12 +8,21 @@ namespace Learning_01_02
 {
     class Challenge
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.Write("Woah we learning better game programming in C#");
-            Console.Write(" And its seems fun");
-            Console.Read();
-            //difference of write and writeline is that writeline makes a new line
+            Console.WriteLine("Press Enter to Start...");
+            Console.ReadLine();
+            Console.WriteLine("Hello, I am Moinuddin Ahmed");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Press enter to Continue...");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine(" I am learning to get better at C# and it is profitably fun");
+            Console.ReadLine();
+            Console.Clear();
+            //difference of write and writeline is that writeline makes a new line.
+            //readline pause the code.
         }
     }
 }
