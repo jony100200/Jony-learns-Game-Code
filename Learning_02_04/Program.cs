74f0e38b663bf225a45ea0a395ea599e76f5fbc1
Jony <gamingbir@gmail.com>
2020-07-18 19:45:57 -0400

    some changes done by vs

---
+++ Learning_02_04/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_02_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string faction = string.Empty;
+            string race = string.Empty;
+            string classType = string.Empty;
+            string gender = string.Empty;
+            //asking the user 2 pick faction
+            Console.WriteLine("Choose ur faction");
+            Console.WriteLine("1.Shadhon\n2.Shenar");
+
+            int input = int.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                case 1:
+                    faction = "Bonno";
+                    break;
+
+                case 2:
+                    faction = "Cival";
+                    break;
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Pick your race");
+            Console.WriteLine("1.Shadonn\n2.Shenar");
+
+            switch (input)
+            {
+                case 1:
+                    race = "Shadhon";
+                    break;
+                case 2:
+                    race = "Shenar";
+                    break;
+            }
+            Console.Clear();
+
+        }
+    }
+}
